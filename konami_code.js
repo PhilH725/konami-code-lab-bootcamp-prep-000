ba076a7d2b4ae53b1ed13768e0e40e5a5a7351a2
Phil Hartley <philh725@gmail.com>
2018-10-14 05:34:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,14 +13,9 @@ const codes = [
 
 function init() {
   // your code here
-  const up = 38
-  const down = 40
-  const left = 37
-  const right = 39
-  const b = 66
-  const a = 65
   document.body.addEventListener('keydown', function(e) {
     console.log(e.which);
   });
+  
 }
 
