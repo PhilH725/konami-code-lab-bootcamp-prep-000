0925ad794fc381de4606f4e923021cda15602e83
Phil Hartley <philh725@gmail.com>
2018-10-14 05:55:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,17 +13,15 @@ const codes = [
 
 function init() {
   // your code here
-  document.body.addEventListener('keydown', function(e) { 
-    const key = e.key; 
-    if ( key === codes[index]) {
-      index++; 
-      if (index === codes.length) { 
-        alert()
-      }} else {
-        index = 0
-      }   });
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    if (key === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        alert() ;
+      }
+    } else {
+      index = 0;
     }
-  } ) ;
-  
+  });
 }
-
